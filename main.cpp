dacb986196699d92feb36c70fef700b3d3bc14b1
DavidingPlus <2065666169@qq.com>
2024-07-10 15:02:53 +0800

    add source code

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "menu.h"
+
+#include <QApplication>
+#include <QFontDatabase>
+#include <QTextEdit>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Menu w;
+    w.show();
+    return a.exec();
+}
